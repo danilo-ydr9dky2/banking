ffcd5bb3f5b2e92b4135a89df1dc22a4b77f2703
Danilo <danilo.ydr9dky2@gmail.com>
2019-07-13 16:43:52 -0300

    Add unit test for sanity checking Accounts

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "accounts count" do
+        assert_equal 2, Account.count
+    end
+
+    test "account one" do
+        assert_equal 100.0, accounts(:one).balance
+    end
+
+    test "account two" do
+        assert_equal 200.0, accounts(:two).balance
+    end
 end
