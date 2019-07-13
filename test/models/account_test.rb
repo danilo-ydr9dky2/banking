b7b6326d200e124036c465ab51463f0520502c68
Danilo <danilo.ydr9dky2@gmail.com>
2019-07-13 17:25:45 -0300

    Add model unit tests

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,15 +1,23 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-    test "accounts count" do
-        assert_equal 2, Account.count
+    test "Frankie's account balances" do
+        assert_equal 0, accounts(:frankie_a0).balance
+        assert_equal 100, accounts(:frankie_a100).balance
+        assert_equal 200, accounts(:frankie_a200).balance
     end
 
-    test "account one" do
-        assert_equal 100.0, accounts(:one).balance
+    test "Frankie's account ownership" do
+        accounts(:frankie_a0, :frankie_a100, :frankie_a200).each do |account|
+            assert_equal users(:frankie), account.user
+        end
     end
 
-    test "account two" do
-        assert_equal 200.0, accounts(:two).balance
+    test "Norma's account balance" do
+        assert_equal 500, accounts(:norma_a500).balance
+    end
+
+    test "Norma's account ownership" do
+        assert_equal users(:norma), accounts(:norma_a500).user
     end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Frankie" do
+      assert_equal 'Frankie', users(:frankie).name
+      assert_equal 'frankie@gmail.com', users(:frankie).email
+      assert_same users(:frankie), users(:frankie).authenticate('lindy')
+  end
+
+  test "Norma" do
+      assert_equal 'Norma', users(:norma).name
+      assert_equal 'norma@gmail.com', users(:norma).email
+      assert_same users(:norma), users(:norma).authenticate('hop')
+  end
 end
